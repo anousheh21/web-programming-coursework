e36609fa29a92f518f153ec7d9982c423cdf387d
anousheh21 <anousheh1@gmail.com>
2024-04-06 10:42:35 +0100

    Tidied up code

--- wedding.php
+++ wedding.php
@@ -57,50 +57,5 @@
     mysqli_close($conn);
 ?>
 
-
-<!-- VENUE CARD TESTING' -->
-<!-- <div class="venue-container">
-  <div class="venue-card">
-        <img class="venue-image" src="imageSeaView.png" alt="Sea View Tavern">
-    <div class="venue-info">
-      <h3>Venue Name</h3>
-      <div class="overview-details">
-        <p>Price: £1000 - £3500</p>
-        <p>Catering: £10 - £25</p>
-        <p>Capacity: 250</p>
-        </div>
-      <a href="singleVenue.php">See More Information</a>
-    </div>
-  </div>
-</div> -->
-
-<!--TESTING READ IN DATABASE-->
-<?php
-    // include "db-config.php";
-    // $conn = mysqli_connect($servername, $username, $password, $dbname);
-
-    // if (!$conn) {
-    //     die("Connectien failed: " . mysqli_connect_error());
-    // }
-
-    // $sql = "select * from venue";
-    // $result = mysqli_query($conn, $sql);
-
-    // echo "Number of results: " . mysqli_num_rows($result);
-
-
-    // $allDatArray = array();
-    // $result = mysqli_query($conn, $sql);
-    // while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-    //     $allDataArray[] = $row;
-    // }
-
-    // echo json_encode($allDataArray);
-
-    // mysqli_close($conn);
-
-    
-?>
-
 </body>
 </html>
