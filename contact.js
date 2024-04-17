3aa61bdf1224d8392939e28f5259cce504640464
anousheh21 <anousheh1@gmail.com>
2024-04-17 10:50:45 +0100

    Added error checking and validation to contact form

--- contact.js
+++ contact.js
@@ -1,3 +1,75 @@
 $(function() {
-    
+    const firstNameInput = $("#contactFormFirstName");
+    const surnameInput = $("#contactFormSurname");
+    const venueInput = $("#contactFormVenue");
+    const dateInput = $("#contactFormDate");
+    const partyInput = $("#contactFormPartySize");
+    const queryInput = $("#contactFormQuery");
+
+   
+
+    let errorPresent = () => {
+        firstNameInput.val(firstName);
+        surnameInput.val(surname);
+        // venueInput.val(venue);
+        dateInput.val(date);
+        partyInput.val(partySize);
+        queryInput.val(query);
+
+        if (venue == "noSelection" || venue == "") {
+            venueInput.val("noSelection");
+        } else {
+            venueInput.val(venue);
+        }
+    }
+
+    if (firstName == "" || surname == "" || date == "" || partySize == "" || venue == "" || venue == "noSelection") {
+        errorPresent()
+    } else {
+        fetch("venueInfo.php")
+        .then(res => res.json())
+        .then(resData => {
+            const contactVenue = resData.filter(item => item.name == venue);
+            contactVenueCapacity = parseInt(contactVenue[0].capacity);
+
+            console.log(contactVenue)
+            console.log(contactVenueCapacity)
+
+            fetch("weddingDates.php")
+                .then(res => res.json())
+                .then(resData => {
+                    const contactDates = resData.filter(item => item.name == venue);
+
+                    // console.log(contactDates);
+                    // console.log(venue);
+                    // console.log(date);
+                    
+                    let bookedVenue = false;
+                    contactDates.forEach(contactDate => {
+                        //console.log(contactDate)
+                        // console.log(date)
+                        
+                        if (contactDate.booking_date == date) {
+                            bookedVenue = true;
+                            return;
+                        } 
+                    })
+
+                    // Contact form validation
+                    if (partySize < 0) {
+                        $("#partySizeError").html("Invalid party size");
+                        errorPresent()
+                    } else if (partySize > contactVenueCapacity) {
+                        $("#partySizeError").html("Party size excdeeds chosen venue capacity");
+                        errorPresent();
+                    } else if (bookedVenue == true) {
+                        $("#dateError").html("Date unavailable");
+                        errorPresent();
+                    }
+
+                })
+        })
+
+    }
 })
+
--- contact.php
+++ contact.php
@@ -27,6 +27,69 @@
 </nav>
 </header>
 
+<?php
+    // Contact form validation
+    $firstNameError = $surnameError = $venueError = $dateError = $partySizeError = "";
+    $firstName = $surname = $venue = $date = $partySize = $query = "";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST["contactFormFirstName"])) {
+            $firstNameError = "First name is required";
+        } else {
+            $firstName = cleanInput($_POST["contactFormFirstName"]);
+        }
+
+        if (empty($_POST["contactFormSurname"])) {
+            $surnameError = "Surname is required";
+        } else {
+            $surname = cleanInput($_POST["contactFormSurname"]);
+        }
+
+        if (empty($_POST["contactFormDate"])) {
+            $dateError = "Wedding date is required";
+        } else {
+            $date = cleanInput($_POST["contactFormDate"]);
+        }
+
+        if (empty($_POST["contactFormPartySize"])) {
+            $partySizeError = "Party size is required";
+        } else {
+            $partySize = cleanInput($_POST["contactFormPartySize"]);
+        }
+
+        if (empty($_POST["contactFormVenue"]) || ($_POST["contactFormVenue"]) == "noSelection") {
+            $venueError = "Venue is required";
+        } else {
+            $venue = cleanInput($_POST["contactFormVenue"]);
+        }
+
+        if (!empty($_POST["contactFormQuery"])) {
+            $query = cleanInput($_POST["contactFormQuery"]);
+        }
+    }
+
+    function cleanInput($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+      }
+?>
+
+<?php
+    // Read venue names from database
+    include "db-config.php";
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    $sql = "select name from venue;";
+    $result = mysqli_query($conn, $sql);
+
+    ?>
+
 <div id="contactPageBody">
     <div id="contactFormHeadingDiv">
         <h1 class="title" id="contactFormHeading">Contact Us</h1>
@@ -35,27 +98,52 @@
     <div id="contactForm">
         <form method="post" action='<?php htmlspecialchars($_SERVER["PHP_SELF"]);?>'>
 
-            <div id="contactFormFirstNameDiv">
+            <div id="contactFormFirstNameDiv" class="contactFormDiv">
                 <label for="contactFormFirstName" class="contactFormInput">First Name</label>
                 <input type="text" id="contactFormFirstName" name="contactFormFirstName">
+                <span class="contactError"><?php echo $firstNameError?></span>
             </div>
 
-            <div id="contactFormSurnameDiv">
+            <div id="contactFormSurnameDiv" class="contactFormDiv">
                 <label for="contactFormSurname" class="contactFormInput">Surname</label>
                 <input type="text" name="contactFormSurname" id="contactFormSurname">
+                <span class="contactError"><?php echo $surnameError?></span>
             </div>
 
-            <div id="contactFormDateDiv">
+            <!-- CHANGE THIS TO A DROPDOWN -->
+            <div id="contactFormVenueDiv" class="contactFormDiv">
+                <label for="contactFormVenue" class="contactFormInput">Venue</label>
+               
+                <select name="contactFormVenue" id="contactFormVenue">
+                    <option value="noSelection" selected>Select</option>
+                    <?php
+                    $result = mysqli_query($conn, $sql);
+                    if (mysqli_num_rows($result) > 0) {
+                        while ($row = mysqli_fetch_array($result)) {
+                            // $selected = ($row['name'] == $dropdownRight) ? 'selected' : '';
+                            echo "<option value='" . $row['name'] . "'>" . $row['name'] . "</option>";
+                        }
+                    }
+                    ?>
+                </select>
+
+                <span class="contactError"><?php echo $venueError?></span>
+            </div>
+
+            <div id="contactFormDateDiv" class="contactFormDiv">
                 <label for="contactFormDate" class="contactFormInput">Wedding Date</label>
                 <input type="date" id="contactFormDate" name="contactFormDate">
+                <span class="contactError" id="dateError"><?php echo $dateError?></span>
             </div>
 
-            <div id="contactFormPartySizeDiv">
+            <div id="contactFormPartySizeDiv" class="contactFormDiv">
                 <label for="contactFormPartySize" class="contactFormInput">Party Size</label>
                 <input type="number" name="contactFormPartySize" id="contactFormPartySize">
+                <span class="contactError" id="partySizeError"><?php echo $partySizeError?></span>
+                <!-- <span id="partySizeError"></span> -->
             </div>
 
-            <div id="contactFormQueryDiv">
+            <div id="contactFormQueryDiv" class="contactFormDiv">
                 <label for="contactFormQuery" class="contactFormInput">Query</label>
                 <textarea name="contactFormQuery" id="contactFormQuery" cols="30" rows="10"></textarea>
             </div>
@@ -63,7 +151,19 @@
             <input type="submit" name="contactFormSubmit" class="buttonStyler" id="contactFormSubmit" value="Submit">
 
         </form>
+
     </div>
+
+    <script>
+        let firstName = "<?php echo"$firstName"?>";
+        let surname = "<?php echo"$surname"?>";
+        let venue = "<?php echo"$venue"?>";
+        let date = "<?php echo"$date"?>";
+        let partySize = "<?php echo"$partySize"?>";
+        let query = "<?php echo"$query"?>";
+    </script>
+
+    <p id="contactFormSubmissionResponse"></p>
 </div>
 
 <script src="contact.js"></script>
